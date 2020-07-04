8402cd380a87c3f73caa52dde993b45ec004bd82
Daniel Chapa <64573238+Chaps235@users.noreply.github.com>
2020-07-04 18:32:09 -0500

    Add files via upload

---
+++ DelitosCDMX.R
@@ -0,0 +1,7 @@
+head(carpetas.de.investigacion.pgj.de.la.ciudad.de.mexico)
+Investigacion_CDMX <- carpetas.de.investigacion.pgj.de.la.ciudad.de.mexico
+Inv_CDMX<-Investigacion_CDMX[c(1,2,3,5,9)]
+Inv_CDMX<-na.omit(Inv_CDMX)
+summary(Inv_CDMX)
+str(Inv_CDMX)
+
